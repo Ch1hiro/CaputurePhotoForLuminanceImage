e3c886d0db38af76a4f046dbaefc7b29768daf7c
Sano Chihiro <sanochhiro.art@gmail.com>
2025-09-05 13:24:13 +0900

    内部でしか使う予定のないEnumの命名を変更

--- theta_wireless.py
+++ theta_wireless.py
@@ -4,24 +4,24 @@ from enum import Enum
 import requests
 
 
-class ThetaConstans(str, Enum):
+class _ThetaConstans(str, Enum):
     THETA_IP = "192.168.1.1"
     EXECUTE_URL = f"http://{THETA_IP}/osc/commands/execute"
     STATUS_URL = f"http://{THETA_IP}/osc/commands/status"
     HEADERS = {"Content-Type": "application/json;charset=utf-8"}
 
 
-class Name(str, Enum):
+class _Name(str, Enum):
     SET_OPTIONS = "camera.setOptions"
     GET_OPTIONS = "camera.getOptions"
     TAKE_PICTURE = "camera.takePicture"
 
 
-class Parameter(str, Enum):
+class _Parameter(str, Enum):
     OPTIONS = "options"
     OPTION_NAMES = "optionNames"
 
-class ThetaWirelessResponse(dict[str,str], Enum):
+class _ThetaWirelessResponse(dict[str,str], Enum):
     TIMEOUT = {"error":"time_out"}
 
 
@@ -36,51 +36,54 @@ def set_options(parameters:dict[str, any]) -> dict[str, any]:
     Thetaへオプションの設定を行う
     """
     payload = _create_payload(
-        Name.SET_OPTIONS,
-        {Parameter.OPTIONS: parameters},
+        _Name.SET_OPTIONS,
+        {_Parameter.OPTIONS: parameters},
     )
 
     try:
         response = requests.post(
-            url=ThetaConstans.EXECUTE_URL,
+            url=_ThetaConstans.EXECUTE_URL,
             json=payload,
-            headers=ThetaConstans.HEADERS,
+            headers=_ThetaConstans.HEADERS,
             timeout=10,
         )
         return response.json
     except requests.exceptions.Timeout:
-        return ThetaWirelessResponse.TIMEOUT
+        return _ThetaWirelessResponse.TIMEOUT
 
 def get_options(option_names:set[str]) -> dict[str, any]:
     """
     Thetaに設定したオプションの情報を取得する
     """
     payload = _create_payload(
-        Name.GET_OPTIONS,
-        {Parameter.OPTION_NAMES:option_names}
+        _Name.GET_OPTIONS,
+        {_Parameter.OPTION_NAMES:option_names}
         )
 
     try:
         response = requests.post(
-            url=ThetaConstans.EXECUTE_URL,
+            url=_ThetaConstans.EXECUTE_URL,
             json=payload,
-            headers=ThetaConstans.HEADERS,
+            headers=_ThetaConstans.HEADERS,
             timeout=10
         )
         return response.json
     except requests.exceptions.Timeout:
-        return ThetaWirelessResponse.TIMEOUT
+        return _ThetaWirelessResponse.TIMEOUT
 
 
 def take_picture() -> dict[str, any]:
     """
     Theta Web APIを用いて撮影
     """
-    payload = _create_payload(Name.TAKE_PICTURE)
+    payload = _create_payload(
+        _Name.TAKE_PICTURE
+        )
+
     response = requests.post(
-        ThetaConstans.EXECUTE_URL, 
+        _ThetaConstans.EXECUTE_URL, 
         json=payload, 
-        headers=ThetaConstans.HEADERS, 
+        headers=_ThetaConstans.HEADERS, 
         timeout=10
         )
     response.raise_for_status()

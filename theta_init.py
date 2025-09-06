7771fc043d0cf75e49aba664d51b21fc55edb84b
Sano Chihiro <sanochhiro.art@gmail.com>
2025-09-07 01:49:37 +0900

    [Update]初期化処理を関数を使って書き直し

--- theta_init.py
+++ theta_init.py
@@ -1,24 +1,30 @@
-"""Script for controlling RICOH THETA camera."""
+"""Thetaカメラを初期化します"""
 
-import requests
+import theta_wireless
+from theta_wireless import Options
 
-# pylint: disable=duplicate-code
+try:
+    theta_wireless.set_options(
+        {
+            Options.CAPTURE_MODE:"image",
+            Options.EXPOSURE_PROGRAM:1,
+            Options.SLEEP_DELAY:65535,
+            Options.SHUTTER_VOLUME:0
+        }
+    )
+    response = theta_wireless.get_options(
+        {
+            Options.CAPTURE_MODE,
+            Options.EXPOSURE_PROGRAM,
+            Options.SLEEP_DELAY,
+            Options.SHUTTER_VOLUME
+        }
+    )
+    print(response.json())
 
-URL = "http://192.168.1.1/osc/commands/execute"
-HEADERS = {"Content-Type": "application/json;charset=utf-8"}
+except theta_wireless.CameraInternalError as e:
+    print(f"Caught OSC API error: {e.code} -> {e.message}")
 
-payload = {
-    "name": "camera.setOptions",
-    "parameters": {"options": {"captureMode": "image"}},
-}
+except theta_wireless.CameraTimeout as e:
+    print("Timeoutしました")
 
-resp = requests.post(URL, json=payload, headers=HEADERS, timeout=10)
-print(resp.json())
-
-payload = {
-    "name": "camera.setOptions",
-    "parameters": {"options": {"exposureProgram": 1}},
-}
-
-resp = requests.post(URL, json=payload, headers=HEADERS, timeout=10)
-print(resp.json())

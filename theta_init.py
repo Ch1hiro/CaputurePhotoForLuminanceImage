d2b11f704c2c5595675b5b9a3430bbf390e43bc1
Sano Chihiro <sanochhiro.art@gmail.com>
2025-09-09 20:07:21 +0900

    [Update]theta_initの処理を関数として外部から参照可能に変更

--- theta_init.py
+++ theta_init.py
@@ -3,27 +3,32 @@
 import theta_wireless
 from theta_wireless import Options
 
-try:
-    theta_wireless.set_options(
-        {
-            Options.CAPTURE_MODE:"image",
-            Options.EXPOSURE_PROGRAM:1,
-            Options.SLEEP_DELAY:65535,
-            Options.SHUTTER_VOLUME:0
-        }
-    )
-    response = theta_wireless.get_options(
-        {
-            Options.CAPTURE_MODE,
-            Options.EXPOSURE_PROGRAM,
-            Options.SLEEP_DELAY,
-            Options.SHUTTER_VOLUME
-        }
-    )
-    print(response)
+def init():
+    """Thetaの初期化処理を行います。"""
+    try:
+        theta_wireless.set_options(
+            {
+                Options.CAPTURE_MODE:"image",
+                Options.EXPOSURE_PROGRAM:1,
+                Options.SLEEP_DELAY:65535,
+                Options.SHUTTER_VOLUME:0
+            }
+        )
+        response = theta_wireless.get_options(
+            {
+                Options.CAPTURE_MODE,
+                Options.EXPOSURE_PROGRAM,
+                Options.SLEEP_DELAY,
+                Options.SHUTTER_VOLUME
+            }
+        )
+        print(response)
 
-except theta_wireless.CameraInternalError as e:
-    print(f"Caught OSC API error: {e.code} -> {e.message}")
+    except theta_wireless.CameraInternalError as e:
+        print(f"Caught OSC API error: {e.code} -> {e.message}")
 
-except theta_wireless.CameraTimeout as e:
-    print("Timeoutしました")
+    except theta_wireless.CameraTimeout as e:
+        print("Timeoutしました")
+
+if __name__ == "__main__":
+    init()
